5bd14b41fa03a9077896301c357bead0bf306903
Josh Crabtree <jcrabtree@tx@outlook.com>
2020-05-19 20:56:43 -0500

    added tls support to run file from local ps window

--- Get-CatFact.ps1
+++ Get-CatFact.ps1
@@ -1,3 +1,4 @@
 $factUrl = 'https://catfact.ninja/fact'
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
 $fact = Invoke-RestMethod $factUrl
 $fact.Fact
